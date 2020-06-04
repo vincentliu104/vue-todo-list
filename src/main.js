9ce110c2127efed91679b5a70a9847c32deb0da2
Vincent Liu <vincent.liu@104.com.tw>
2020-06-04 17:08:49 +0800

    add template syntax

--- src/main.js
+++ src/main.js
@@ -4,7 +4,15 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
-new Vue({
+// vm0.a = 'this is a pencil'
+const data = { a: 'this is an apple' };
+
+// Vue Instance
+const vm = new Vue({
+  // options https://vuejs.org/v2/api/#Options-Data
   store,
   render: (h) => h(App),
+  data,
 }).$mount('#app');
+
+console.log(vm);
