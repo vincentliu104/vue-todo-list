cc5583d266475d8d35cb610a884819400377e2ac
Vincent Liu <vincent.liu@104.com.tw>
2020-06-21 21:04:00 +0800

    mapGetters

--- src/components/todo/List.vue
+++ src/components/todo/List.vue
@@ -4,25 +4,29 @@
 </template>
 
 <script>
-// import { mapState } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
 
 export default {
   name: 'TodoList',
-  computed: {
-    todos() {
-      return this.$store.state.todos;
-    },
-    doneTodosCount() {
-      return this.$store.getters.doneTodosCount;
-    },
-    no1() {
-      return this.$store.getters.getTodoById(1);
-    },
-  },
-  // computed: mapState({ // vuex binding
-  //   todos: (state) => state.todos,
-  //   countAlias: 'count', // countAlias: (state) => state.count
-  // }),
+  // computed: {
+  // todos() {
+  //   return this.$store.state.todos;
+  // },
+  // doneTodosCount() {
+  //   return this.$store.getters.doneTodosCount;
+  // },
+  // no1() {
+  //   return this.$store.getters.getTodoById(1);
+  // },
+  // },
+  computed: mapState({ // vuex binding
+    todos: (state) => state.todos,
+    countAlias: 'count', // countAlias: (state) => state.count
+    ...mapGetters([
+      'doneTodosCount',
+      // ...
+    ]),
+  }),
   // computed: mapState([
   // // 映射 this.count 为 store.state.count
   //   'todos',
