5d5a688c91527446236499bc9c94c4a9f59ba370
Vincent Liu <vincent.liu@104.com.tw>
2020-06-21 20:59:02 +0800

    getters

--- src/components/todo/List.vue
+++ src/components/todo/List.vue
@@ -4,23 +4,26 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+// import { mapState } from 'vuex';
 
 export default {
   name: 'TodoList',
-  // computed: {
-  //   todos() {
-  //     return this.$store.state.todos;
-  //   },
-  // },
+  computed: {
+    todos() {
+      return this.$store.state.todos;
+    },
+    doneTodosCount() {
+      return this.$store.getters.doneTodosCount;
+    },
+  },
   // computed: mapState({ // vuex binding
   //   todos: (state) => state.todos,
   //   countAlias: 'count', // countAlias: (state) => state.count
   // }),
-  computed: mapState([
-  // 映射 this.count 为 store.state.count
-    'todos',
-    'count',
-  ]),
+  // computed: mapState([
+  // // 映射 this.count 为 store.state.count
+  //   'todos',
+  //   'count',
+  // ]),
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -6,10 +6,14 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     todos: [
-      { id: 1, title: 'do1', done: false },
+      { id: 1, title: 'do1', done: true },
       { id: 2, title: 'do1', done: false },
     ],
     count: 2,
+  },
+  getters: {
+    doneTodos: (state) => state.todos.filter((todo) => todo.done),
+    doneTodosCount: (state, getters) => getters.doneTodos.length,
   },
   mutations: {
   },
